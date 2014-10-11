312ad004df300a3bb60f1ee8c28034e4c66c6006
David Fabreguette <dfabreguette@gmail.com>
2014-10-11 16:06:26 +0200

    ...

--- app/models/instrument_preference.rb
+++ app/models/instrument_preference.rb
@@ -2,7 +2,7 @@ class InstrumentPreference < ActiveRecord::Base
 
   belongs_to :user
 
-  enum instrument: %w(violin trombone trumpet piano acoustic_bass acoustic_guitar electric_guitar electric_bass_guitar drums bongos flute saxophone oboe)
+  enum instrument: %w(voice violin trombone trumpet piano acoustic_bass acoustic_guitar electric_guitar electric_bass_guitar drums bongos flute saxophone oboe)
 
 
 end
