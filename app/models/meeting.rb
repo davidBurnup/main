77be947e73db01c839d08f749f3a1c6bd7bbaca8
David Fabreguette <dfabreguette@gmail.com>
2014-10-11 16:17:09 +0200

    fixed : bad var name

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -47,7 +47,7 @@ class Meeting < ActiveRecord::Base
   def plan_practice_notifications
     if practice
       diff_in_seconds = practice.start_at.to_i - Time.now.to_i
-      MeetingWorker.perform_in(diff_in_seconds.seconds,self.id)
+      MeetingsWorker.perform_in(diff_in_seconds.seconds,self.id)
     end
   end
 
