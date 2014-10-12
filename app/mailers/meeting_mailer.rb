965cf2991c151156464c0b2674c7bb6afc436773
David Fabreguette <dfabreguette@gmail.com>
2014-10-12 15:48:31 +0200

    ;..

--- app/mailers/meeting_mailer.rb
+++ app/mailers/meeting_mailer.rb
@@ -5,8 +5,8 @@ class MeetingMailer < ActionMailer::Base
     @user = meeting_user.user
     @meeting = meeting_user.meeting
     if @user and @user.email and @meeting
-      attachments.inline['logo_mini4.svg'] = File.read('/assets/logo_mini4.svg')
-      attachments.inline['user_pic'] = File.read(@uesr.avatar.url(:med_tiny))
+      attachments.inline['logo_mini4.svg'] = File.read(Rails.root.join('app', 'assets', 'images', 'logo_mini4.svg'))
+      attachments.inline['user_pic'] = File.read(@user.avatar.path(:med_tiny))
       mail(to: @user.email, subject: "Réunion \"#{@meeting.label}\" pour le #{@meeting.start_at.strftime("%d/%m/%Y à %H:%M")}" )
     end
   end
