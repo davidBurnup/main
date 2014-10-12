2723b45acdfe8eb91bcda29d03ffdfc27fd8f02c
David Fabreguette <dfabreguette@gmail.com>
2014-10-12 07:50:33 +0200

    ...

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,6 @@
 #initializers/sidekiq.rb
 schedule_file = "config/schedule.yml"
 
-if File.exists?(schedule_file)
+if File.exists?(schedule_file) and YAML.load_file(schedule_file)
   Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
 end
