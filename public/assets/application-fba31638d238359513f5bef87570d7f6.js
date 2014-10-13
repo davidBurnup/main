2e44db8e565a4c218ed9f2779e827ce6a78dd23c
David Fabreguette <dfabreguette@gmail.com>
2014-10-13 22:48:37 +0200

    ...

--- public/assets/application-fba31638d238359513f5bef87570d7f6.js
+++ public/assets/application-fba31638d238359513f5bef87570d7f6.js
@@ -18493,6 +18493,22 @@ THE SOFTWARE.
 
 
 (function() {
+  jQuery(function() {
+    if ($('#infinite-scrolling').size() > 0) {
+      return $(window).on('scroll', function() {
+        var more_posts_url;
+        more_posts_url = $('.pagination .next_page a').attr('href');
+        if (more_posts_url && $(window).scrollTop() > $(document).height() - $(window).height() - 60) {
+          $('.pagination').html('<img src="/assets/ajax-loader.gif" alt="Loading..." title="Loading..." />');
+          $.getScript(more_posts_url);
+        }
+        return;
+      });
+    }
+  });
+
+}).call(this);
+(function() {
   this.global_ready = function() {
     var ref_el;
     $('[data-behaviour~=datepicker]').datetimepicker({
@@ -18675,6 +18691,7 @@ THE SOFTWARE.
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
 
 
 
