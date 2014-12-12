33df2993e8ab782b31970bcc318acf6ba05ebac1
David Fabreguette <dfabreguette@gmail.com>
2014-12-13 00:17:08 +0100

    ..

--- config/initializers/setup_email.rb
+++ config/initializers/setup_email.rb
@@ -5,7 +5,7 @@ if Rails.env.production?
     :port      => 587, # ports 587 and 2525 are also supported with STARTTLS
     :enable_starttls_auto => true, # detects and uses STARTTLS
     :user_name => "dfabreguette@gmail.com",
-    :password  => "uhpV0YrqtJ9Lz7oPShYj8A", # SMTP password is any valid API key
+    :password  => Rails.application.secrets.mandrill_api_key, # SMTP password is any valid API key
     :authentication => 'login', # Mandrill supports 'plain' or 'login'
     :domain => 'burnup.ac-ac.fr', # your domain to identify your server when connecting
   }
