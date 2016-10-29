8267b4be70bd6824900cc77816d540d628cbe640
David Fabreguette <dfabreguette@gmail.com>
2016-10-29 15:48:14 +0200

    ...

--- config/initializers/setup_email.rb
+++ config/initializers/setup_email.rb
@@ -4,9 +4,9 @@ if Rails.env.production?
     :address   => "smtp.mailgun.org",
     :port      => 587, # ports 587 and 2525 are also supported with STARTTLS
     :enable_starttls_auto => true, # detects and uses STARTTLS
-    :user_name => "postmaster@ac-ac.fr",
+    :user_name => "postmaster@sandbox57f9a0eb31734214b5839beda27c123a.mailgun.org",
     :password  => Rails.application.secrets.mailgun_api_key, # SMTP password is any valid API key
     :authentication => 'login', # Mandrill supports 'plain' or 'login'
-    :domain => 'ac-ac.fr', # your domain to identify your server when connecting
+    :domain => 'sandbox57f9a0eb31734214b5839beda27c123a.mailgun.org', # your domain to identify your server when connecting
   }
 end
