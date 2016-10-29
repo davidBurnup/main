8e56a76f381aa5fac18df662ae20aa49dc95899c
David Fabreguette <dfabreguette@gmail.com>
2016-10-29 15:26:26 +0200

    fixed: mailgun email

--- config/initializers/setup_email.rb
+++ config/initializers/setup_email.rb
@@ -1,12 +1,12 @@
 if Rails.env.production?
   ActionMailer::Base.default_url_options[:host] = "burnup.ac-ac.fr"
   ActionMailer::Base.smtp_settings = {
-    :address   => "smtp.mandrillapp.com",
+    :address   => "smtp.mailgun.org",
     :port      => 587, # ports 587 and 2525 are also supported with STARTTLS
     :enable_starttls_auto => true, # detects and uses STARTTLS
-    :user_name => "dfabreguette@gmail.com",
-    :password  => Rails.application.secrets.mandrill_api_key, # SMTP password is any valid API key
+    :user_name => "postmaster@ac-ac.fr",
+    :password  => Rails.application.secrets.mailgun_api_key, # SMTP password is any valid API key
     :authentication => 'login', # Mandrill supports 'plain' or 'login'
-    :domain => 'burnup.ac-ac.fr', # your domain to identify your server when connecting
+    :domain => 'ac-ac.fr', # your domain to identify your server when connecting
   }
 end
