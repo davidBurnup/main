44234de35ba17e5cc4d97303bcb03dbbeaddd03e
David Fabreguette <dfabreguette@gmail.com>
2016-12-20 21:51:51 +0100

    ..

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,15 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # config.autoload_paths << Rails.root.join('lib')
-  
+
   config.ng_annotate.process = true
+
+  Rails.application.config.middleware.use ExceptionNotification::Rack,
+  :email => {
+    :deliver_with => :deliver, # Rails >= 4.2.1 do not need this option since it defaults to :deliver_now
+    :email_prefix => "[BUNRUP ERRORS] ",
+    :sender_address => %{"David FABREGUETTE" <david@ac-ac.fr>},
+    :exception_recipients => %w{dfabreguette@gmail.com}
+  }
 
 end
