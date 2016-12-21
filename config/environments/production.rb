487f009a960cf64ff5eb8db4ca3a2f9b64ca039a
David Fabreguette <dfabreguette@gmail.com>
2016-12-21 23:11:44 +0100

    ...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,6 +74,8 @@ Rails.application.configure do
 
   # ActionMailer Config
   config.action_mailer.default_url_options = { :host => "http://burnup.ac-ac.fr" }
+    config.action_mailer.asset_host = "http://burnup.ac-ac.fr"
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
