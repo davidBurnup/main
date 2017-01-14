88f172223e5544604f49d2fa8ffd0f6cae0a3bc2
David Fabreguette <dfabreguette@gmail.com>
2017-01-14 21:38:31 +0100

    ...

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :deploy_to, "/var/www/burnup.fr"
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml", "config/secrets.yml", "config/sidekiq.yml"
+append :linked_files, "config/database.yml", "config/secrets.yml", "config/sidekiq.yml", "config/cable.yml"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
