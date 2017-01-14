232121b3330998aac165f3e91132683916649ed5
David Fabreguette <dfabreguette@gmail.com>
2017-01-14 07:53:08 +0100

    ...

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, "git@github.com:davidfabreguette/burnup.git"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/home/david/burnup"
+set :deploy_to, "/var/www/burnup"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server "burnup.ac-ac.fr", user: "david", roles: %w{app}
+server "burnup.fr", user: "deploy", roles: %w{app}
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
@@ -17,7 +17,7 @@ server "burnup.ac-ac.fr", user: "david", roles: %w{app}
 # property set. Specify the username and a domain or IP for the server.
 # Don't use `:all`, it's a meta role.
 
-role :app, %w{david@burnup.ac-ac.fr}
+role :app, %w{contact@burnup.fr}
 # role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
 # role :db,  %w{deploy@example.com}
 
